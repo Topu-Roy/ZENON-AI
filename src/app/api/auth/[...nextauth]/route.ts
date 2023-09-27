9b7e633df36d9647148889d05e5f80088dd11c11
Topu-Roy <topuroy143333@gmail.com>
2023-09-28 02:34:14 +0600

    Next Auth Credentials ongoing...

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,34 @@
-import { AuthOptions } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { Prisma } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-export const authOptions: AuthOptions = {};
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        password: { label: "Password", type: "password" },
+        email: { label: "Email", type: "text", placeholder: "john@app.com" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
