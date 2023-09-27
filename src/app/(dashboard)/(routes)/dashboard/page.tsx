2509d43901291177ac901fa0546bc4610ba56a06
Topu-Roy <topuroy143333@gmail.com>
2023-09-27 21:38:30 +0600

    DEP

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+"use client";
+import { useUser } from "@clerk/nextjs";
 
-export default function Dashboard() {
-    return (
-        <div>Dashboard</div>
-    )
+export default function Example() {
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    if (!isLoaded || !isSignedIn) {
+        return null;
+    }
+
+    return <div>Hello, {user.firstName} welcome to Clerk</div>;
 }
